3669607fe8e6bb29aef4d30ccc3d37df00fbfb5b
Vinothkumar <Vinothkumar.n@tavant.com>
2024-10-18 18:53:07 +0530

    secondcommit

--- SEB-Refinance/run.bat
+++ SEB-Refinance/run.bat
@@ -1,4 +1,4 @@
 echo off
-cd C:\Users\vinothkumar.n\PROJECT\Equifax\Sprint40\Equifax 2024 Upadted project\Refinance project updated\SEB-Refinance\
+cd C:\Users\vinothkumar.n\PROJECT\Remote Repository SEB Project\SEB-Refinance\
 C:
 mvn clean test -Dxml=custom -Denv=custom
